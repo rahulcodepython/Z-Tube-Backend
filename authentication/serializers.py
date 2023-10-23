7d1337de5d87e72199d7ead381097400f123ab09
Rahul Das <77882624+rahulcodepython@users.noreply.github.com>
2023-10-24 02:25:35 +0530

    Custom jwt token claims are removed

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -1,21 +1,9 @@
-from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from djoser.serializers import UserSerializer, UserCreateSerializer
 from django.contrib.auth import get_user_model
 from djoser.conf import settings
 from . import create_username
 
 User = get_user_model()
-
-
-class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
-    def validate(self, attrs):
-        data = super().validate(attrs)
-        data['first_name'] = self.user.first_name
-        data['last_name'] = self.user.last_name
-        data['email'] = self.user.email
-        data['username'] = self.user.username
-        data['is_superuser'] = self.user.is_superuser
-        return data
 
 
 class CustomUserSerializer(UserSerializer):
