d73870cc424c6c9f9a3acb0eccdbe072abe9dff9
Rahul Das <77882624+rahulcodepython@users.noreply.github.com>
2023-10-30 21:34:27 +0530

    Extra Spaces are removed

--- authentication/views.py
+++ authentication/views.py
@@ -3,6 +3,8 @@ from rest_framework import permissions
 from rest_framework import views, response
 from . import serializers, models
 from django.contrib.auth import get_user_model
+
+
 User = get_user_model()
 
 
